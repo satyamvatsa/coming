68da327d315798444702a9be35411f450b8c9130
satyam vatsa <66280108+satyamvatsa@users.noreply.github.com>
2023-06-25 00:33:07 +0530

    new

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 function App() {
   return (
     <>
-      <div>
-        hello
-      </div>
+      <section className='header'>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-12 d-flex justify-content-center bg-dark py-3 text-white main_nav'>
+              hello
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
