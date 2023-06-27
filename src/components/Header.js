8ad054bcad302d5717c0b5cb88b192daeb695304
satyam vatsa <66280108+satyamvatsa@users.noreply.github.com>
2023-06-27 14:22:23 +0530

    nav

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <>
+    <div className='container-fluid'>
+        <div className='row'>
+            <div className='col-12 p-4 nav_main'>
+                hello
+            </div>
+        </div>
+    </div>
+    </>
+  );
+};
+
+export default Header
